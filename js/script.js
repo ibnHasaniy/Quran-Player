9a7e329ea0b53fc6c55b61d42d9b0be28063002f
ibn Hasaniy <76680356+ibnHasaniy@users.noreply.github.com>
2021-04-15 01:20:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,45 @@ const progressContainer = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
-//Song Titles
-const songs = ['Al - Fatiha', 'Ar - Rahman'];
+// const songs = ['Al - Fatiha', 'Ar - Rahman'];
+const songs = [
+  'Al - Fatiha', 'Ar - Rahman',
+  "80. 'Abasa",
+  "81. At-Takwir",
+  "82. Al-Infitar",
+  "83. Al-Mutaffifin",
+  "84. Al-Inshiqaq",
+  "85. Al-Buruj",
+  "86. At-Tariq",
+  "87. Al-A'la",
+  "88. Al-Ghashiyah",
+  "89. Al-Fajr",
+  "90. Al-Balad",
+  "91. Ash-Shams",
+  "92. Al-Layl",
+  "93. Adh-Dhuha",
+  "94. Al-Inshirah",
+  "95. At-Tin",
+  "96. Al-'Alaq",
+  "97. Al-Qadr",
+  "98. Al-Baiyinah",
+  "99. Al-Zalzalah",
+  "100. Al-'Adiyat",
+  "101. Al-Qari'ah",
+  "102. At-Takathur",
+  "103. Al-'Asr",
+  "104. Al-Humazah",
+  "105. Al-Fil",
+  "106. Quraish",
+  "107. Al-Ma'un",
+  "108. Al-Kauthar",
+  "109. Al-Kafirun",
+  "110. An-Nasr",
+  "111. Al-Masad",
+  "112. Al-Ikhlas",
+  "113. Al-Falaq",
+  "114. An-Nas",
+];
 
 //Keep track of songs
 let songIndex = 1;
